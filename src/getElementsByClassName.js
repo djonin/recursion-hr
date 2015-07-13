cd6df88c0f96aa45b21c470d7171f66a1503546a
Dmitrijs Jonins <randomnickname1@gmail.com>
2015-07-13 15:13:48 -0700

    solved getelements

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,9 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  var returnArr = [];
+var getElementsByClassName = function(className) {
+  
+	function recursiveGetElements(obj) {
+		var result = [];
 
-  return returnArr;
+		if(obj.classList == undefined)
+			return result;
+
+		for (var i = 0; i<obj.classList.length; i++) {
+			if(className === obj.classList[i]) {
+				result.push(obj);
+				break;
+			}
+		}
+
+		if(obj.childNodes == undefined)
+			return result;
+
+		for (var j =0; j<obj.childNodes.length; j++) {
+			result = result.concat(recursiveGetElements(obj.childNodes[j]));
+		}
+
+		return result;
+	}
+
+  return recursiveGetElements(document.body);
 };
